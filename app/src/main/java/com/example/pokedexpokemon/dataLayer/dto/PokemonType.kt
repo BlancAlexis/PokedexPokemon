3fceabfc74acb6a3abf36bfdfc69266678ebd552
Alexis <blanc.alexispro@gmail.com>
2024-07-21 07:30:57 +0200

    feat(api_call_describe_pokemon): add type enum

---
+++ app/src/main/java/com/example/pokedexpokemon/dataLayer/dto/PokemonType.kt
@@ -0,0 +1,22 @@
+package com.example.pokedexpokemon.dataLayer.dto
+
+enum class PokemonType {
+    BUG,
+    DARK,
+    DRAGON,
+    ELECTRIC,
+    FAIRY,
+    FIGHTING,
+    FIRE,
+    FLYING,
+    GHOST,
+    GRASS,
+    GROUND,
+    ICE,
+    NORMAL,
+    POISON,
+    PSYCHIC,
+    ROCK,
+    STEEL,
+    WATER
+}
