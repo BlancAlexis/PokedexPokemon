022b610e737f7f65ffe394036345bc727ebd9a63
Alexis <blanc.alexispro@gmail.com>
2024-07-23 23:18:41 +0200

    feat(query_card_pokemon): add lazyGrid extraPane

--- app/src/main/java/com/example/pokedexpokemon/presentationLayer/listDetailScreen/extraCardPokemon/ExtraCardScreen.kt
+++ app/src/main/java/com/example/pokedexpokemon/presentationLayer/listDetailScreen/extraCardPokemon/ExtraCardScreen.kt
@@ -1,13 +1,24 @@
 package com.example.pokedexpokemon.presentationLayer.listDetailScreen.extraCardPokemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pokedexpokemon.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -20,13 +31,18 @@ ExtraCardScreen()
 fun ExtraCardScreen(
 
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.tertiaryContainer),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "content")
+    LazyVerticalGrid(columns = GridCells.Fixed(2), contentPadding = PaddingValues(vertical = 20.dp)) {
+        items(40){
+            Column(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .background(Color.Blue)
+            ) {
+                Image(painter = painterResource(id = R.drawable.pika_splash_screen) , contentDescription = "" )
+
+            }
+HorizontalDivider()
+        }
     }
 }
 
