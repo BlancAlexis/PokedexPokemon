60d982882d20d77e6c812f728cd14c736ff207b7
Alexis <blanc.alexispro@gmail.com>
2025-01-03 10:45:52 +0100

    chore(): upgrade dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply true
+    id("com.google.devtools.ksp")
 
 }
 
--- app/src/main/java/com/example/pokedexpokemon/presentationLayer/BaseViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.pokedexpokemon.presentationLayer
-
-class BaseViewModel {
-}
